08a4d1021c565b9c6195bc31fb2e8cc713420368
Katerrio Kennedy <kkennedy@basecampcodingacademy.org>
2026-08-08 11:31:02 -0500

    more informationon the database

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,5 @@
 import sqlite3
-def setup_database():
+def setup_database(): #sets up my database, Kat
     connection = sqlite3.conncet("Scoreboard.db")
     cursor = connection.cursor()
 
@@ -9,6 +9,32 @@ def setup_database():
                     player_score INTERGER
                     )               
                 """)
-    input_name = input("ENTER NAME: ")
-    input_player_score = int(input("ENTER SCORE: "))
-    insert_query = "INSERT INTO Scoreboard (player_name, player_score)"
+    
+    connection.commit()
+    connection.close()
+    
+def add_player_name(player_name, player_score): #should allow the user to input a name to go along with their score
+    connection = sqlite3.conncet("Scoreboard.db")
+    cursor = connection.cursor()
+    
+    cursor.execute
+    insert_query = "INSERT INTO Scoreboard (player_name, player_score) VALUES(?, ?)",
+    (player_name, player_score)
+    
+    connection.commit()
+    connection.close()
+    
+def get_top_scores(): #should get the top ten scores and show them from the database, Kat
+    connection = sqlite3.conncet("Scoreboard.db")
+    cursor = connection.cursor()
+    
+    cursor.execute(""" 
+                   SELECT player_name, player_score
+                   FROM scores
+                   ORDER BY player_score DESC
+                   LIMIT 10
+                   """)
+    scores = cursor.fetchall()
+    
+    connection.close()
+    return scores
