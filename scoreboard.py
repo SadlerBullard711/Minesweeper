a1e334dd91a3a7ff4135809d880d8f0ab397ca20
Katerrio Kennedy <kkennedy@basecampcodingacademy.org>
2026-08-08 12:03:58 -0500

    small error fixes

--- scoreboard.py
+++ scoreboard.py
@@ -14,7 +14,7 @@ def setup_database(): #sets up my database, Kat
     connection.close()
     
 def add_player_name(player_name, player_score): #should allow the user to input a name to go along with their score, Kat
-    connection = sqlite3.conncet("Scoreboard.db")
+    connection = sqlite3.connect("Scoreboard.db")
     cursor = connection.cursor()
     
     cursor.execute
@@ -25,7 +25,7 @@ def add_player_name(player_name, player_score): #should allow the user to input 
     connection.close()
     
 def get_top_scores(): #should get the top ten scores and show them from the database, Kat
-    connection = sqlite3.conncet("Scoreboard.db")
+    connection = sqlite3.connect("Scoreboard.db")
     cursor = connection.cursor()
     
     cursor.execute(""" 
