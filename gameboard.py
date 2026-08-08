39441253e44f2dc270fea20eb6541d64a9add13d
Sadler Bullard <sbullard@basecampcodingacademy.org>
2026-08-08 13:37:37 -0500

    "Changed the number of mines."

--- gameboard.py
+++ gameboard.py
@@ -23,7 +23,7 @@ class Board:
         mines = 0
         mines_max = len(self.board[0]) * len(self.board[0])
         #mines_num is the number of mines to place.
-        mines_num = 50
+        mines_num = 20
         #if the set number of mines is greater than spaces on the board, shrink the number of mines.
         if mines_num > mines_max:
             mines_num = mines_max
