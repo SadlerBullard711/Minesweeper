77fa113c62420141efb4ce072102fe95a2d69abb
jslack7 <jslack@basecampcodingacademy.org>
2026-08-08 15:45:25 -0500

    updates

--- play.py
+++ play.py
@@ -41,22 +41,24 @@ def play_game():
         row, column = get_coordinates()
         action = get_action()
 
-        cell = board.board[column][row]
+        cell = board.board[row][column]
 
         if action == "r":
             if cell.flagged:
                 print("This cell is flagged. Unflag it before revealing.")
                 continue
-            coordinate = [row,column]
+
+            coordinate = [row, column]
             cell.reveal(board)
 
-            if cell.number == 0:
-                return reveal_adjacent(board, coordinate)
-
+            
             if cell.mine:
                 board.draw_board()
                 print("Game Over! You hit a mine!")
                 break
+
+            if cell.number == 0:
+                reveal_adjacent(board, coordinate)
 
             if board.unrevealed == 0:
                 end_time = time.time()
@@ -82,66 +84,66 @@ def play_game():
 def reveal_adjacent(board: Board, coordinate): #coordinate is a list [row, column]
     if (coordinate[0] != 0) and (coordinate[1] != 0):
         top_left = board.board[coordinate[0] - 1][coordinate[1] - 1]
-        if not top_left.revealed:
-            top_left.reveal()
+        if not top_left.revealed and not top_left.mine:
+            top_left.reveal(board)
             if top_left.number == 0:
                 new_coordinate = [coordinate[0] - 1, coordinate[1] - 1]
                 reveal_adjacent(board, new_coordinate)
 
     if (coordinate[0] != 0) and (coordinate[1] != board.size - 1):
         top_right = board.board[coordinate[0] - 1][coordinate[1] + 1]
-        if top_right.revealed:
-            top_right.reveal()
+        if not top_right.revealed and not top_right.mine:
+            top_right.reveal(board)
             if top_right.number == 0:
                 new_coordinate = [coordinate[0] - 1, coordinate[1] + 1]
                 reveal_adjacent(board, new_coordinate)
 
     if (coordinate[0] != 0):
         top_middle = board.board[coordinate[0] - 1][coordinate[1]]
-        if top_middle.revealed:
-            top_middle.reveal()
+        if not top_middle.revealed and not top_middle.mine:
+            top_middle.reveal(board)
             if top_middle.number == 0:
                 new_coordinate = [coordinate[0] - 1, coordinate[1]]
                 reveal_adjacent(board, new_coordinate)
             
     if (coordinate[0] != board.size - 1) and (coordinate[1] != 0):
         bottom_left = board.board[coordinate[0] + 1][coordinate[1] - 1]
-        if bottom_left.revealed:
-            bottom_left.reveal()
+        if not bottom_left.revealed and not bottom_left.mine:
+            bottom_left.reveal(board)
             if bottom_left.number == 0:
                 new_coordinate = [coordinate[0] + 1, coordinate[1] - 1]
                 reveal_adjacent(board, new_coordinate)
             
     if (coordinate[0] != board.size - 1) and (coordinate[1] != board.size -1):
         bottom_right = board.board[coordinate[0] + 1][coordinate[1] + 1]
-        if bottom_right.revealed:
-            bottom_right.reveal()
+        if not bottom_right.revealed and not bottom_right.mine:
+            bottom_right.reveal(board)
             if bottom_right.number == 0:
                 new_coordinate = [coordinate[0] + 1, coordinate[1] + 1]
                 reveal_adjacent(board, new_coordinate)
 
     if (coordinate[0] != board.size - 1):
         bottom_middle = board.board[coordinate[0] + 1][coordinate[1]]
-        if bottom_middle.revealed:
-            bottom_middle.reveal()
+        if not bottom_middle.revealed and not bottom_middle.mine:
+            bottom_middle.reveal(board)
             if bottom_middle.number == 0:
                 new_coordinate = [coordinate[0] + 1, coordinate[1]]
                 reveal_adjacent(board, new_coordinate)
 
     if (coordinate[1] != 0):
         middle_left = board.board[coordinate[0]][coordinate[1] - 1]
-        if middle_left.revealed:
-            middle_left.reveal()
+        if not middle_left.revealed and not middle_left.mine:
+            middle_left.reveal(board)
             if middle_left.number == 0:
-                new_coordinate = [coordinate[0] + 1, coordinate[1] - 1]
+                new_coordinate = [coordinate[0] , coordinate[1] - 1]
                 reveal_adjacent(board, new_coordinate)
 
     if (coordinate[1] != board.size - 1):
         middle_right = board.board[coordinate[0]][coordinate[1] + 1]
-        if middle_right.revealed:
-            middle_right.reveal()
+        if not middle_right.revealed and not middle_right.mine:
+            middle_right.reveal(board)
             if middle_right.number == 0:
-                new_coordinate = [coordinate[0] + 1, coordinate[1] - 1]
+                new_coordinate = [coordinate[0] , coordinate[1] +1]
                 reveal_adjacent(board, new_coordinate)
 
                 
