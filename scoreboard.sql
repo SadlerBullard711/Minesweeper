725dbef957f355a9a415454a7d1b370de4605bf0
Katerrio Kennedy <kkennedy@basecampcodingacademy.org>
2026-08-08 09:06:11 -0500

    created table and database

--- scoreboard.sql
+++ scoreboard.sql
@@ -1,2 +1,7 @@
--- scoreboard database for minesweeper project
-CREATE TABLE IF NOT EXISTS Scoreboard (player_name varchar(3), player_score int)
+-- scoreboard database for minesweeper project, Kat
+CREATE TABLE IF NOT EXISTS Scoreboard (player_name varchar(3), player_score int);
+
+INSERT or IGNORE INTO Scoreboard(player_name, player_score)
+VALUES('KJK', 12);
+
+SELECT * FROM Scoreboard;
